4a3316fc72be8efd6bb6d303ff89f194adc1b3e9
asjonats <161716948+asjonats@users.noreply.github.com>
2024-02-29 13:49:08 +0000

    2

--- analysis.py
+++ analysis.py
@@ -1,24 +1,16 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 
-# โหลดข้อมูล
-url = "https://raw.githubusercontent.com/asjonats/Baankidteng/0219e55fcd4a1151ddc1427b35abaa31248d3f17/Bann%20Kidteng%20reservation%20-%20reservations%20on%20now.csv"
-data = pd.read_csv(url)
+# โหลดข้อมูล CSV
+url = "https://github.com/asjonats/Baankidteng/raw/68c0f1b862644220c5ee4f8db32141d90948bf7e/Bann%20Kidteng%20reservation%20-%20reservations%20on%20now.csv"
+df = pd.read_csv(url)
 
-# แปลงคอลัมน์ 'date' เป็นชนิดข้อมูล datetime
-data['date'] = pd.to_datetime(data['date'])
 
-# สร้างตารางเป็น index โดยใช้คอลัมน์ 'date'
-data.set_index('date', inplace=True)
+print(df['dateIn'].dtypes)
 
-# หาจำนวนการจองในแต่ละเดือน
-monthly_bookings = data.resample('M').size()
+# แปลงคอลัมน์ 'reserve_date' เป็นรูปแบบวันที่
+df['date'] = pd.to_datetime(df['dateIn'])
 
-# พล็อตกราฟแสดงแนวโน้มของจำนวนการจองตามเดือน
-plt.figure(figsize=(10, 6))
-plt.plot(monthly_bookings)
-plt.title('Monthly Bookings')
-plt.xlabel('Month')
-plt.ylabel('Number of Bookings')
-plt.grid(True)
-plt.show()
+print(df.head())
+print(df.dtypes)
+
+
